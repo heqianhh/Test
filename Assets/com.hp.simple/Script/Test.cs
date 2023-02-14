7aa8989996e447a2a793e97df404596104fa17ab
heqian <1831056611@qq.com>
2023-02-14 12:39:05 +0800

    初始化

---
+++ Assets/com.hp.simple/Script/Test.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Test {
+    public static int Add(this int Num, int addNum) {
+        return Num + addNum;
+    }
+
+}
